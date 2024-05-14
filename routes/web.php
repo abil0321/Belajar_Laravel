e4af02f0c019f71ea8eeb17c07e4ae1b1188247d
abil0321 <salsabilmuhammad45@gmail.com>
2024-05-15 06:55:11 +0700

    sampai materi controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
 
 // Route::view('/', 'welcome');
@@ -10,18 +11,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn () => view('welcome'));//->name("welcome");
 
-Route::get('/about', fn () => view('about'));//->name("welcome");
+Route::get('/home', Controllers\HomeController::class);//->name("welcome");
 
-Route::get('/galeri', fn () => view('galeri'));//->name("welcome");
+Route::get('/about', [Controllers\AboutController::class, 'index']);//->name("welcome");
 
-Route::get('/home', fn () => view('home'));//->name("welcome");
+Route::get('/galeri', [Controllers\GaleriController::class, 'index']);//->name("welcome");
 
-Route::get('/contact', fn () => view('contact'));//->name("welcome");
+Route::get('/contact', [Controllers\ContactController::class, 'index']);//->name("welcome");
 
 Route::get('users', function () {
     $users = [
         ['id' => 1, 'name' => 'john doe', 'email' => 'johndoe@gmail.com'],
-        
+
         ['id' => 2, 'name' => 'jane doe', 'email' => 'janedoe@gmail.com'],
     ];
     return view('users.index', compact('users'));

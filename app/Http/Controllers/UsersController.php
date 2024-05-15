53994ba397f1da57917df1427539c0094f817d02
abil0321 <salsabilmuhammad45@gmail.com>
2024-05-16 05:50:21 +0700

    study laravel model

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,32 +10,6 @@ class UsersController extends Controller
 {
     public function index()
     {
-        // $users = DB::table("users")->get();
 
-        // $users = DB::table("users")->limit(1)->get();
-        // $users = DB::table("users")->take(1)->get();
-
-        // $users = DB::table("users")->get()->toArray();
-
-        // $users = DB::table("users")->find(1);
-
-        // $users = DB::table("users")->where('email', 'salsaabilmuhammad45@gmail.com')->first();
-        // $users = DB::table("users")->where('email', 'like', '%@yahoo.com')->get();
-        // $users = DB::table("users")->where('email', 'like', '%@gmail.com')->first();
-
-        // $users = DB::table("users")
-        // ->where('email', 'like', '%@gmail.com')
-        // ->latest() // orderBy('id', 'desc')
-        // ->oldest() // orderBy('id', 'asc')
-        // ->first();
-
-        $users = DB::table("users")
-        ->where('id', 2) // find(2)
-        ->first();
-
-        // return $users;
-        // return $users->email;
-
-        dd($users);
     }
 }

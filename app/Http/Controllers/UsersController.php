5b685dde898cc3aab1ec5b726656dc1c9edc65c1
abil0321 <salsabilmuhammad45@gmail.com>
2024-05-16 23:29:18 +0700

    model dan fetching data

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use \Illuminate\Support\Facades\DB;
@@ -10,6 +11,40 @@ class UsersController extends Controller
 {
     public function index()
     {
+        // return User::findMany([
+        //     1
+        // ]);
 
+        // return User::query()->firstWhere('email', 'splash@yahoo.com');
+
+        // return User::query()
+        // ->where('email', 'splash@yahoo.com')
+        // ->latest()
+        // ->get()
+        // ->map(fn ($user) =>[
+        //     'name' => $user->name,
+        // ]);
+
+        // return User::query()
+        // ->where('email', 'splash@yahoo.com')
+        // ->latest()
+        // ->get();
+
+        // return DB::table("articles")
+        // ->select('title', 'slug', 'id')
+        // ->get();
+
+        // return DB::table("articles")
+        // ->get();
+
+        $users = User::query()
+        ->latest()
+        ->get();
+
+        // return view("users.index", compact("users"));
+
+        return view("users.index", [
+            "people" => $users
+        ]);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,10 +1,34 @@
-<x-app-layout>
+<x-app-layout title="Users">
     <x-slot name="heading">
         Users
     </x-slot>
     <h1>Users Content</h1>
-    @foreach ($users as $user)
-        <h1>{{ $user['name'] }}</h1>
-        <p>{{ $user['email'] }}</p>
-    @endforeach
+    @dump($people)
+
+    <table>
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>name</th>
+                <th>email</th>
+                <th>created at</th>
+                <th>created at</th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- @foreach ($people as $index => $user) --}}
+            @foreach ($people as $user)
+                <tr>
+                    {{-- <td>{{ ++$index }}</td> --}}
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->created_at->format('d M Y') }}</td>
+                    <td>{{ $user->created_at->diffForHumans() }} </td>
+                    <td>{{ (new \Carbon\Carbon($user->created_at))->format('d M Y') }}</td>
+
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-app-layout>

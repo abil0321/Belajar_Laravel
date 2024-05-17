7e234585f73772fe777f47c6d51eca0123a4e812
abil0321 <salsabilmuhammad45@gmail.com>
2024-05-17 07:19:41 +0700

    styling model dan fetching result yesterday

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,33 +2,50 @@
     <x-slot name="heading">
         Users
     </x-slot>
-    <h1>Users Content</h1>
-    @dump($people)
+    {{-- @dump($people) --}}
 
-    <table>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>name</th>
-                <th>email</th>
-                <th>created at</th>
-                <th>created at</th>
-            </tr>
-        </thead>
-        <tbody>
-            {{-- @foreach ($people as $index => $user) --}}
-            @foreach ($people as $user)
+    <div class="sm:flex sm:items-center">
+        <x-section-title>
+            <x-slot name="title">Users</x-slot>
+            <x-slot name="description">A list of all the users in your account including their name,
+                title,
+                email and role.
+            </x-slot>
+        </x-section-title>
+        <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <x-button type="button">
+                Add user
+            </x-button>
+        </div>
+    </div>
+    <div class="mt-8 flow-root">
+        <x-table>
+            <x-table.thead>
                 <tr>
-                    {{-- <td>{{ ++$index }}</td> --}}
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $user->name }}</td>
-                    <td>{{ $user->email }}</td>
-                    <td>{{ $user->created_at->format('d M Y') }}</td>
-                    <td>{{ $user->created_at->diffForHumans() }} </td>
-                    <td>{{ (new \Carbon\Carbon($user->created_at))->format('d M Y') }}</td>
-
+                    <x-table.th>#</x-table.th>
+                    <x-table.th>name</x-table.th>
+                    <x-table.th>email</x-table.th>
+                    <x-table.th>created at</x-table.th>
+                    <x-table.th>at</x-table.th>
+                    <x-table.th>created at</x-table.th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </x-table.thead>
+            <x-table.tbody>
+                {{-- @foreach ($people as $index => $user) --}}
+                @foreach ($people as $user)
+                    <tr>
+                        {{-- <td>{{ ++$index }}</td> --}}
+                        <x-table.td>{{ $loop->iteration }}</x-table.td>
+                        <x-table.td>{{ $user->name }}</x-table.td>
+                        <x-table.td>{{ $user->email }}</x-table.td>
+                        <x-table.td>{{ $user->created_at->format('d M Y') }}</x-table.td>
+                        <x-table.td>{{ $user->created_at->diffForHumans() }} </x-table.td>
+                        <x-table.td>{{ (new \Carbon\Carbon($user->created_at))->format('d F Y') }}</x-table.td>
+
+                    </tr>
+                @endforeach
+            </x-table.tbody>
+        </x-table>
+    </div>
+
 </x-app-layout>
